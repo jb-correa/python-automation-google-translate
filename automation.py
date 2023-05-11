d1417ec928658d8bb89edc7ef9edc54cd676aece
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-05-11 19:04:03 -0300

    Trabajo de prueba en automation con selenium

--- automation.py
+++ automation.py
@@ -1,3 +1,20 @@
-import webbrowser
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+from xlutils.copy import copy
+import re
+import xlwt
+import time
+import xlrd
+from webdriver_manager.chrome import ChromeDriverManager
 
-webbrowser.open("www.google.com", 2, autoraise=True)
+driver = webdriver.Chrome()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem= driver.find_element(By.NAME, 'q')
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
